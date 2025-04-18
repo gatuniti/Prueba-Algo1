8a3e29b7a264579193bbeb81c58e3faaf97a9b58
gatuniti <vdiiorio@estudiantes.unsam.edu.ar>
2025-04-18 17:42:37 -0300

    Ejercicio "Contador de Palabras Especial"

---
+++ Introduccion/src/ContadorPalabras.java
@@ -0,0 +1,20 @@
+//Ejercicio Contador de palabras especial
+package Introduccion.src;
+
+import java.util.Scanner;
+
+public class ContadorPalabras {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese una frase: ");
+        String frase = scanner.nextLine();
+
+        System.out.println("Ingrese un separador: ");
+        String separador = scanner.nextLine();
+
+        String[] palabras = frase.split(separador);
+
+        System.out.println("Cantidad de palabras: " + palabras.length);
+    }
+}
