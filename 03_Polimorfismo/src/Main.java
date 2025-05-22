e46eda49cea13bbd98addc0af38bd034ce9eae64
gatuniti <vdiiorio@estudiantes.unsam.edu.ar>
2025-05-22 17:45:54 -0300

    Chore: Reordené todo (again)

--- 03_Polimorfismo/src/Main.java
+++ 03_Polimorfismo/src/Main.java
@@ -1,15 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package Polimorfismo;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Pruebas ejercicio "Biblioteca de medios":
+//        Libro libro = new Libro("El Gran Gatsby", "F. Scott Fitzgerald");
+//        Pelicula pelicula = new Pelicula("La La Land", "Damien Chazelle");
+//        Musica musica = new Musica("Bohemian Rhapsody", "Queen");
+//
+//        Medio[] listaMedios = {libro, pelicula, musica};
+//
+//        for(Medio medio: listaMedios){
+//            medio.reproducir();
+//        }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        //Pruebas ejercicio "Calculadora":
+//        CalculadoraAvanzada calculadora = new CalculadoraAvanzada();
+//        int a = calculadora.sumar(1,2);
+//        double b = calculadora.sumar(1.5, 8.6);
+//        int c = calculadora.restar(8, 9);
+//        double d = calculadora.restar(8.4, 1.2);
+//        int e = calculadora.multiplicar(7, 7);
+//        double f = calculadora.multiplicar(2.2, 3.6);
+//        double g = calculadora.dividir(7, 8);
+//        double h = calculadora.dividir(5.3, 8.1);
+//        double i = calculadora.dividir(1, 0);
+//        System.out.println(i);
     }
 }
